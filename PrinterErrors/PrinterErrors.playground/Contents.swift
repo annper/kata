aedbea67a41763ea5aca774dc109c535e11163a4
A <annie.mc.persson@gmail.com>
2019-04-29 15:26:11 +0100

    kata ( PrinterErrors )

--- PrinterErrors/PrinterErrors.playground/Contents.swift
+++ PrinterErrors/PrinterErrors.playground/Contents.swift
@@ -1,3 +1,38 @@
-import UIKit
+import Foundation
+import XCTest
 
-var str = "Hello, playground"
+// MARK: - Kata
+
+func errorPrinter(controlString: String) -> String {
+  let goodChars = "abcdefghijklm"
+  let badCount = controlString.filter({ !goodChars.contains($0) }).count
+  
+  return "\(badCount)/\(controlString.count)"
+}
+
+// MARK: - Tests
+
+class Tests: XCTestCase {
+  
+  func testErrorPrinterWithAllGoodControlString() {
+    let result = errorPrinter(controlString: "aaabbbbhaijjjm")
+    XCTAssertEqual(result, "0/14")
+  }
+  
+  func testErrorPrinterWithEmptyControlString() {
+    let result = errorPrinter(controlString: "")
+    XCTAssertEqual(result, "0/0")
+  }
+  
+  func testErrorPrinterWithAllBadControlString() {
+    let result = errorPrinter(controlString: "nopqrrst")
+    XCTAssertEqual(result, "8/8")
+  }
+  
+  func testErrorPrintWithSomeBadControlString() {
+    let result = errorPrinter(controlString: "aabbcczzfpqa")
+    XCTAssertEqual(result, "4/12")
+  }
+}
+
+Tests.defaultTestSuite.run()
