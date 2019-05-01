337099635b076a0378e00713c92387cc55d61428
A <annie.mc.persson@gmail.com>
2019-05-01 16:58:28 +0100

    refactor ( use lambda and filter)

--- PrinterErrors/PrinterErrors.py
+++ PrinterErrors/PrinterErrors.py
@@ -1,16 +1,12 @@
-def printerErrors(controlString):
-    acceptedLetters = "abcdefghijklm"
-    
-    filteredString = ''
-    for letter in controlString:
-        if letter not in acceptedLetters:
-            filteredString += letter
+
+def printerErrors(controlString):    
+    removeAcceptedLetters = lambda letter: letter not in "abcdefghijklm"
+    filteredString = ''.join(filter(removeAcceptedLetters, list(controlString)))
     
     return f'{len(filteredString)}/{len(controlString)}'
 
 def printControlString(controlString):
     print(printerErrors(controlString))
-
 
 printControlString("aaabbbffgggiia")
 printControlString("pqrrzzxtuuq")
